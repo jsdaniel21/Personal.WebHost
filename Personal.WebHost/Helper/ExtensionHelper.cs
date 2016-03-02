fb2f49f0ad6241aeabe1aab695d6239990b03c59
jhoel daniel salinas <jsdaniel_21@hotmail.com>
2016-03-01 19:13:51 -0500

    5

--- Personal.WebHost/Helper/ExtensionHelper.cs
+++ Personal.WebHost/Helper/ExtensionHelper.cs
@@ -5,11 +5,32 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Profile;
 using Personal.Utilities;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+using System.Linq.Expressions;
 
 namespace Personal.WebHost.Helper
 {
-    public static class fechasHelper
+    public static class ExtensionHelper
     {
+        public static MvcHtmlString DropDownListFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, IEnumerable<SelectListItem> selectList, string optionLabel, object htmlAttributes, bool disabled)
+        {
+            Func<TModel, TProperty> method = expression.Compile();
+            string value = method(htmlHelper.ViewData.Model) as string;
+
+            var attributes = new RouteValueDictionary(htmlAttributes);
+            if (disabled)
+                attributes["disabled"] = "disabled";
+
+            return htmlHelper.DropDownListFor(expression, selectList, optionLabel, attributes, disabled);
+        }
+        public static MvcHtmlString TextBoxFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, object htmlAttributes, bool disabled)
+        {
+            var attributes = new RouteValueDictionary(htmlAttributes);
+            if (disabled)
+                attributes["disabled"] = "disabled";
+            return htmlHelper.TextBoxFor(expression, attributes);
+        }
         public static MvcHtmlString selectDia(this HtmlHelper helper, Dictionary<string, string> attr)
         {
             string contents = "<option>Dia</option>";
